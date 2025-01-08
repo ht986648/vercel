7d1e1f2352749896a470cf50ea9f33ca9f72f1f2
Himanshu Tiwari <ht976648@gmail.com>
2025-01-08 12:31:02 +0530

    commited my changes

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+
+import {Navbar} from './components/Navbar.tsx'
+import {Hero} from './components/Hero.tsx'
+// import {NewCourses} from './components/Navbar.tsx'
+// import {ExistingCourses} from './components/Navbar.tsx'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <Navbar/>
+      <Hero/>
+      {/* <NewCourses/>
+      <ExistingCourses/> */}
+    </>
+  )
+}
+
+export default App
